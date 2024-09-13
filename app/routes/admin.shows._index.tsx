63ce0b6dc15f3eecb0c80e6c0be1bed067a9d8ec
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-09-13 20:44:37 +0300

    ✨ Fetched Shows in Admin page

--- app/routes/admin.shows._index.tsx
+++ app/routes/admin.shows._index.tsx
@@ -10,21 +10,36 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 export default () => {
   const shows = useLoaderData<typeof loader>();
   return (
-    <div className="flex flex-col">
-      <Link to="/admin/shows/add" className="underline">
-        New Show
-      </Link>
+    <>
+      <div className="flex flex-col">
+        <Link to="/admin/shows/add" className="underline">
+          New Show
+        </Link>
+      </div>
       {shows.length === 0 ? (
         <p>No shows found</p>
       ) : (
-        <table>
+        <table className="w-full border-white *:border">
           <thead>
             <tr>
               <th>Name</th>
             </tr>
+            <tr>
+              <th>Description</th>
+            </tr>
           </thead>
+          <tbody>
+            {shows.map((show) => (
+              <tr key={show.id}>
+                <td>
+                  <Link to={`/admin/shows/${show.id}`}>{show.name}</Link>
+                </td>
+                <td>{show.description}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       )}
-    </div>
+    </>
   );
 };
--- app/routes/admin.shows.tsx
+++ app/routes/admin.shows.tsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from "@remix-run/react";
 
 export default () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex">
       <h1 className="text-3xl">Shows</h1>
       <Outlet />
     </div>
